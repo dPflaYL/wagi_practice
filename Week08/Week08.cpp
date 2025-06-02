91ec87f84e8d72b58915b6a4863dab68f7ab0338
dPflaYL <20241020@sungshin.ac.kr>
2025-06-02 14:44:42 +0900

    Add files via upload

---
+++ Week08/Week08.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int reverse(int num) {
+    int result = 0;
+    while (num != 0) {
+        result = result * 10 + num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+int main() {
+    int num;
+    cout << "정수를 입력하세요: ";
+    cin >> num;
+    cout << "역순 결과: " << reverse(num) << endl;
+    return 0;
+}
